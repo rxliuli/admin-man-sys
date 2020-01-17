53b7b0e5d0b0f119632184e501e8810866866f7c
rxliuli <rxliuli@gmail.com>
2020-01-17 13:33:22 +0800

    实现顶部标题栏

--- src/pages/base/layout/LayoutHeader.tsx
+++ src/pages/base/layout/LayoutHeader.tsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd'
+import { Avatar, Badge, Dropdown, Icon, Layout, Menu } from 'antd'
+import styles from './LayoutHeader.module.css'
 
 const { Header } = Layout
 
 class LayoutHeader extends Component {
   render() {
-    return <Header style={{ background: '#fff' }}>顶部标题栏</Header>
+    return (
+      <Header
+        className={styles.layoutHeader}
+        style={{ backgroundColor: '#fff' }}
+      >
+        <Badge count={5}>
+          <Icon type="bell" style={{ fontSize: 24 }} />
+        </Badge>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={0}>
+                <Icon type="info-circle" />
+                个人信息
+              </Menu.Item>
+              <Menu.Item key={1}>
+                <Icon type="logout" />
+                退出
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Avatar icon="user" style={{ marginLeft: 8 }} />
+        </Dropdown>
+      </Header>
+    )
   }
 }
 
