aa726b324641c60a93c5aa9da95adae9ae806796
rxliuli <rxliuli@gmail.com>
2020-02-18 09:03:25 +0800

    使用 hooks 重构列表过滤器组件

--- src/components/list/component/FilterTimeRange.tsx
+++ src/components/list/component/FilterTimeRange.tsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import moment, { Moment } from 'moment'
 import { DatePicker } from 'antd'
 import FilterBase from './FilterBase'
+import { useComputed } from '../../hooks/useComputed'
 
 const { RangePicker } = DatePicker
 
@@ -17,21 +18,28 @@ type PropsType = {
   onChange: (values: [Moment, Moment] | [undefined, undefined]) => void
 }
 
-class FilterTimeRange extends Component<PropsType> {
-  render() {
-    const value = (this.props.value || []).map(val => val && moment(val)) as [
+/**
+ * 日期选择器
+ * @param props
+ * @constructor
+ */
+const FilterTimeRange: React.FC<PropsType> = props => {
+  const [innerValue] = useComputed(() => {
+    const res = (props.value || []).map(val => val && moment(val)) as [
       Moment,
       Moment,
     ]
-    return (
-      <FilterBase title={this.props.title}>
-        <RangePicker
-          value={value}
-          onChange={value => this.props.onChange(value as any)}
-        />
-      </FilterBase>
-    )
-  }
+    props.onChange && props.onChange(res)
+    return res
+  }, [props.value])
+  return (
+    <FilterBase title={props.title}>
+      <RangePicker
+        value={innerValue}
+        onChange={value => props.onChange(value as any)}
+      />
+    </FilterBase>
+  )
 }
 
 export default FilterTimeRange
