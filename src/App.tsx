4cf80d6ab154ac41847df1fc128c6b6633c703be
rxliuli <rxliuli@gmail.com>
2020-05-16 15:57:16 +0800

    注册 serviceWorker

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import LayoutHome from './pages/base/layout/LayoutHome'
 import './App.css'
 import { Route, Switch } from 'react-router'
 import Login from './pages/system/auth/Login'
+import { useDidMount } from './common/hooks/useDidMount'
 
 const App: React.FC = () => {
+  useDidMount(() => {
+    if (process.env.NODE_ENV === 'development') {
+      const { registerObserver } = require('react-perf-devtool')
+      // assign the observer to the global scope, as the GC will delete it otherwise
+      Reflect.set(window, 'observer', registerObserver())
+    }
+  })
   return (
     <div className="app">
       <Switch>
